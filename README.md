cda0a933c9239e4d87ea0e1dc6b028b65125b745
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-03-26 15:21:25 +0900

    STEP 1. 완료  - Product class    - field 작성    - getter & setter 작성  - main class    - printMenu 메서드 추가    - numberInput 작성 + 잘못된 입력(숫자가 아니거나 리스트 밖의 수)에 대한 예외처리

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ### Main.java
  - 시작 지점이 되는 클래스
+ - printMenu(List<Product>) -> 메뉴를 출력해주는 함수
+ - selectNumber(Scanner, int, String) -> 숫자 입력을 받는 함수(예외 처리 포함)
 
 ### Product.java
  - 개별 상품 정보를 가지는 클래스
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,42 @@
 public class Product {
+    private String name, description;
+    private int price, quantity;
+
+    Product(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
